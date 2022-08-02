ec9cd404b6f063836e1a6328e65d113a2f2527f8
Kyoorim <kyoorim88@gmail.com>
2022-08-02 16:57:57 +0900

    graphql 완료

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 import { ApolloServer, gql } from "apollo-server";
+import fetch from "node-fetch";
 
 let tweets = [
-  { id: "1", text: "first one" },
-  { id: "2", text: "second one" },
+  { id: "1", text: "first one", userId: "2" },
+  { id: "2", text: "second one", userId: "1" },
+];
+
+let users = [
+  {
+    id: "1",
+    firstName: "Kyoorim",
+    lastName: "LEE",
+  },
+  {
+    id: "2",
+    firstName: "Elon",
+    lastName: "Musk",
+  },
 ];
 
 //gql애개 데이터의 타입을 설명하는 코드 SDL (Schema Definition Language)
@@ -13,20 +27,55 @@ const typeDefs = gql`
     username: String!
     firstName: String!
     lastName: String!
+    """
+    Is the sum of firstname and lastname a string
+    """
+    fullName: String!
   }
-
+  """
+  Tweet object represents a resource for a Tweet
+  """
   type Tweet {
     id: ID!
     text: String!
     author: User
   }
   type Query {
+    allMovies: [Movie!]!
+    allUsers: [User!]!
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
+    movie(id: String!): Movie
   }
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
+    """
+    Deletes a Tweet if found, else returns false
+    """
     deleteTweet(id: ID!): Boolean!
+  }
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String
+    description_full: String!
+    synopsis: String
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
   }
 `;
 
@@ -39,12 +88,27 @@ const resolvers = {
       // 첫번째 인자는 항상 root, (root, args)하고 console.log(args)하면 console에 {id:'1'}가 찍힘
       return tweets.find((tweet) => tweet.id === id);
     },
+    allUsers() {
+      console.log("allUsers called");
+      return users;
+    },
+    allMovies() {
+      return fetch("https://yts.mx/api/v2/list_movies.json")
+        .then((r) => r.json())
+        .then((json) => json.data.movies);
+    },
+    movie(root, { id }) {
+      return fetch("https://yts.mx/api/v2/movie_details.json?movie_id=${id}")
+        .then((r) => r.json())
+        .then((json) => json.data.movie);
+    },
   },
   Mutation: {
     postTweet(root, { text, userId }) {
       const newTweet = {
         id: tweets.length + 1,
         text,
+        userId,
       };
       tweets.push(newTweet);
       return newTweet;
@@ -54,6 +118,16 @@ const resolvers = {
       if (!tweet) return false;
       tweets = tweets.filter((tweet) => tweet.id !== id);
       return true;
+    },
+  },
+  User: {
+    fullName({ firstName, lastName }) {
+      return `${firstName} ${lastName}`;
+    },
+  },
+  Tweet: {
+    author({ userId }) {
+      return users.find((user) => user.id === userId);
     },
   },
 };
